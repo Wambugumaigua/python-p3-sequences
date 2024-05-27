aa6157b22702b765d1a4ec22e437eb7aa39593af
Alex Wambugu <wambugumaigua@DESKTOP-F32N3MJ>
2024-05-27 00:18:13 -0700

    complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])
+        return
+    
+    fibonacci_sequence = [0]
+    if length == 1:
+        print(fibonacci_sequence)
+        return
+    
+    fibonacci_sequence.append(1)
+    while len(fibonacci_sequence) < length:
+        fibonacci_sequence.append(fibonacci_sequence[-1] + fibonacci_sequence[-2])
+    print(fibonacci_sequence)
